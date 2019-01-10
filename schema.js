0aa2131d3f009c5043c3701e79e85b2bfd7aabf4
phaybein <fabian.m.martinez@gmail.com>
2019-01-09 17:43:25 -0800

    adding mutation addcustomer RE #mutation #post

--- schema.js
+++ schema.js
@@ -64,6 +64,50 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+// MUTATIONS
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addCustomer: {
+      type: CustomerType,
+      args: {
+        name: {
+          type: new GraphQLNonNull(GraphQLString)
+        },
+        email: {
+          type: new GraphQLNonNull(GraphQLString)
+        },
+        age: {
+          type: new GraphQLNonNull(GraphQLInt)
+        }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .post(`http://localhost:3000/customers`, {
+            name: args.email,
+            email: args.email,
+            age: args.age
+          })
+          .then(res => res.data);
+      }
+    },
+    deleteCustomer: {
+      type: CustomerType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString)
+        }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .delete(`http://localhost:3000/customers/${args.id}`)
+          .then(res => res.data);
+      }
+    }
+  }
+});
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 });
